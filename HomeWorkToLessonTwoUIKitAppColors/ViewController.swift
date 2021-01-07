43c1d6b9e8d329e7e88804bcbf9874c1d97088fa
Vladimir <kynicin87@mail.ru>
2021-01-07 14:38:01 +0200

    Initial Commit

---
+++ HomeWorkToLessonTwoUIKitAppColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkToLessonTwoUIKitAppColors
+//
+//  Created by vladimir gennadievich on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mainImageColorMixOtlet: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainImageColorMixOtlet.layer.cornerRadius = 10
+    }
+
+
+}
+
